18ebe6529572bd9d0f4dc5f3e7a641f1b12c33b1
Roman Agaltsev <roman-agalcev@yandex.ru>
2026-06-19 13:11:03 +0300

    feat(recipe): compose discipline sub-modules into go-service + refresh golden

--- internal/render/golden_test.go
+++ internal/render/golden_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/RomanAgaltsev/keel"
 	"github.com/RomanAgaltsev/keel/internal/answers"
 	"github.com/RomanAgaltsev/keel/internal/module"
+	"github.com/RomanAgaltsev/keel/internal/recipe"
 	"github.com/RomanAgaltsev/keel/internal/render"
 )
 
@@ -19,15 +20,21 @@ var update = flag.Bool("update", false, "update golden files")
 
 func TestGoServiceGolden(t *testing.T) {
 	l := module.NewFSLoader(keel.BuiltinFS)
-	plan, err := render.BuildRecipe(l, []string{"base-layout", "go-mod"}, answers.Answers{
-		"repo_name":   "demo",
-		"description": "a demo service",
-		"module_path": "github.com/RomanAgaltsev/demo",
+	rec, err := recipe.Load(keel.BuiltinFS, "go-service")
+	require.NoError(t, err)
+
+	plan, err := render.BuildRecipe(l, rec.Modules, answers.Answers{
+		"repo_name":          "demo",
+		"description":        "a demo service",
+		"module_path":        "github.com/RomanAgaltsev/demo",
+		"enable_codeql":      true,
+		"enable_govulncheck": true,
+		"enable_codecov":     false,
+		"dep_bot":            "dependabot",
 	})
 	require.NoError(t, err)
 
 	goldenDir := filepath.Join("testdata", "golden", "go-service")
-
 	if *update {
 		require.NoError(t, os.RemoveAll(goldenDir))
 		require.NoError(t, render.WritePlan(plan, goldenDir))
